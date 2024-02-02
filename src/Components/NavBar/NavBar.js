655dc00a134b58827d920e00d89b023d05139ed9
Mohamed Abdelhay <93220261+MohamedAbdelhay00@users.noreply.github.com>
2024-02-02 19:37:27 +0200

    3rd commit

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,39 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 import "./NavBar.css";
 
 export default function NavBar() {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const navbarStyle = {
+    padding: scrollPosition > 80 ? '5px 10px' : '10px',
+    transition: 'padding 1s ease',
+  };
+
+  const logoStyle = {
+    fontSize: scrollPosition > 80 ? '25px' : '27px',
+    transition: 'font-size 1s ease',
+  };
+
   return (
-    <nav className="navbar nav-custom navbar-expand-lg text-light p-4">
-      <div className="container">
-        <NavLink className="nav-link logo" to="">
+    <nav  className="navbar nav-custom navbar-expand-lg text-light p-4 position-fixed w-100 z-3">
+      <div id="navbar" style={navbarStyle} className="container">
+        <NavLink id="logo" style={logoStyle} className="nav-link logo" to="">
           START FRAMEWORK
         </NavLink>
         <button
