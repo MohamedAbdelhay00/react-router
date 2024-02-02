1672aba9e6968a0b59db24ce86ffd6bb37acc439
Mohamed Abdelhay <93220261+MohamedAbdelhay00@users.noreply.github.com>
2024-02-02 18:56:21 +0200

    2nd commit

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import './NotFound.css'
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='not-found text-center'>
+      <i class="fa-regular fa-face-frown"></i>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <p>The page you are looking for doesn't exist or an other error occurred.</p>
+      <p>Go back, or head over to home page to choose a new direction.</p>
+    </div>
   )
 }
